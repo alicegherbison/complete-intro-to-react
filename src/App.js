180b6963f5a035fe4e4a9c48d95137de3a979f88
alicegherbison <git@alicegherbison.com>
2019-10-24 11:03:22 +0100

    build: add prettierrc

--- src/App.js
+++ src/App.js
@@ -1,30 +1,30 @@
 const Pet = ({ name, animal, breed }) => {
-  return React.createElement("div", {}, [
-    React.createElement("h1", {}, name),
-    React.createElement("h2", {}, animal),
-    React.createElement("h2", {}, breed)
-  ]);
-};
+  return React.createElement('div', {}, [
+    React.createElement('h1', {}, name),
+    React.createElement('h2', {}, animal),
+    React.createElement('h2', {}, breed),
+  ])
+}
 
 const App = () => {
-  return React.createElement("div", {}, [
-    React.createElement("h1", {}, "Adopt me!"),
+  return React.createElement('div', {}, [
+    React.createElement('h1', {}, 'Adopt me!'),
     React.createElement(Pet, {
-      name: "Magic",
-      animal: "cat",
-      breed: "moggie"
+      name: 'Magic',
+      animal: 'cat',
+      breed: 'moggie',
     }),
     React.createElement(Pet, {
-      name: "Garfield",
-      animal: "cat",
-      breed: "ginger"
+      name: 'Garfield',
+      animal: 'cat',
+      breed: 'ginger',
     }),
     React.createElement(Pet, {
-      name: "Ollie",
-      animal: "cat",
-      breed: "polite"
-    })
-  ]);
-};
+      name: 'Ollie',
+      animal: 'cat',
+      breed: 'polite',
+    }),
+  ])
+}
 
-ReactDOM.render(React.createElement(App), document.getElementById("root"));
+ReactDOM.render(React.createElement(App), document.getElementById('root'))
