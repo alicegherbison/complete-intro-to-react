86b5b64e55969cf86d83c130092e08c98f658ae5
alicegherbison <git@alicegherbison.com>
2019-10-25 09:52:11 +0100

    build: add eslint configs and plugins

--- src/Pet.js
+++ src/Pet.js
@@ -1,11 +1,13 @@
 import React from 'react';
 
 const Pet = ({ name, animal, breed }) => {
-  return React.createElement('div', {}, [
-    React.createElement('h1', {}, name),
-    React.createElement('h2', {}, animal),
-    React.createElement('h2', {}, breed),
-  ]);
+  return (
+    <>
+      <h2>{name}</h2>
+      <h3>{animal}</h3>
+      <h3>{breed}</h3>
+    </>
+  );
 };
 
 export default Pet;
