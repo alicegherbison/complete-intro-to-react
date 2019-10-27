a926d5e4e9fd3f37d9ee2f954a52c3309cb083ec
alicegherbison <git@alicegherbison.com>
2019-10-27 17:32:34 +0000

    build: install api

--- src/SearchParameters.js
+++ src/SearchParameters.js
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import { ANIMALS } from '@frontendmasters/pet';
 
 const SearchParameters = () => {
   const [location, setLocation] = useState('Seattle, WA');
