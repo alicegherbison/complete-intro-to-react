a2ccbae8a66d1f42fd68cab5d12e83d61955aec9
alicegherbison <git@alicegherbison.com>
2019-10-27 17:41:03 +0000

    feat: add animal dropdown

--- src/SearchParameters.js
+++ src/SearchParameters.js
@@ -3,6 +3,7 @@ import { ANIMALS } from '@frontendmasters/pet';
 
 const SearchParameters = () => {
   const [location, setLocation] = useState('Seattle, WA');
+  const [animal, setAnimal] = useState('cat');
 
   return (
     <div className="search-parameters">
@@ -14,6 +15,21 @@ const SearchParameters = () => {
             onChange={event => setLocation(event.target.value)}
             value={location}
           />
+        </label>
+        <label htmlFor="animal">
+          Animal
+          <select
+            id="animal"
+            onBlur={event => setAnimal(event.target.value)}
+            onChange={event => setAnimal(event.target.value)}
+            value={animal}>
+            <option>All</option>
+            {ANIMALS.map(animal => (
+              <option key={animal} value={animal}>
+                {animal}
+              </option>
+            ))}
+          </select>
         </label>
         <button>Submit</button>
       </form>
