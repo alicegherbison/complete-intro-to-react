87d07ab11e0b5ebcba49bde092090d1928cbb9f0
alicegherbison <git@alicegherbison.com>
2019-11-03 15:23:02 +0000

    feat: update pet component

--- src/Pet.js
+++ src/Pet.js
@@ -1,11 +1,23 @@
 import React from 'react';
 
-const Pet = ({ name, animal, breed }) => {
+const Pet = ({ animal, breed, id, location, media, name }) => {
+  let hero = 'http://placekitten.com/300/300';
+
+  if (media.length) {
+    hero = media[0].small;
+  }
+
   return (
     <>
-      <h2>{name}</h2>
-      <h3>{animal}</h3>
-      <h3>{breed}</h3>
+      <a href={'/details/${id}'} className="pet">
+        <div className="image-container">
+          <img src={hero} alt={name} />
+        </div>
+      </a>
+      <div className="info">
+        <h1>{name}</h1>
+        <h2>{`${animal} - ${breed} - ${location}`}</h2>
+      </div>
     </>
   );
 };
