517d18a19c407490cea72528ecf3c29d7138bbfc
alicegherbison <git@alicegherbison.com>
2019-11-03 15:33:50 +0000

    feat: begin adding routes

--- src/App.js
+++ src/App.js
@@ -1,14 +1,18 @@
 import React from 'react';
 import { render } from 'react-dom';
-import { router } from '@reach/router';
+import { Router } from '@reach/router';
 
+import Details from './Details';
 import SearchParameters from './SearchParameters';
 
 const App = () => {
   return (
     <React.StrictMode>
       <h1>Adopt me!</h1>
-      <SearchParameters />
+      <Router>
+        <SearchParameters path="/" />
+        <Details path="/details/:id" />
+      </Router>
     </React.StrictMode>
   );
 };
