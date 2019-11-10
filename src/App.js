23ebf94bc2d2991d6c184c9664b151256224fa1b
alicegherbison <git@alicegherbison.com>
2019-11-10 12:46:36 +0000

    feat: add reach router

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { render } from 'react-dom';
-import { Router } from '@reach/router';
+import { Link, Router } from '@reach/router';
 
 import Details from './Details';
 import SearchParameters from './SearchParameters';
@@ -8,7 +8,9 @@ import SearchParameters from './SearchParameters';
 const App = () => {
   return (
     <React.StrictMode>
-      <h1>Adopt me!</h1>
+      <header>
+        <Link to="/">Adopt me!</Link>
+      </header>
       <Router>
         <SearchParameters path="/" />
         <Details path="/details/:id" />
