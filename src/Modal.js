e36fb4765838841ab6eceee6933519da0b348c66
alicegherbison <git@alicegherbison.com>
2019-11-23 16:35:42 +0000

    feat: use createportal to add a modal

---
+++ src/Modal.js
@@ -0,0 +1,24 @@
+import React, { useEffect, useRef } from 'react';
+import { createPortal } from 'react-dom';
+
+const Modal = ({ children }) => {
+  const elementReference = useRef(null);
+
+  if (!elementReference.current) {
+    const div = document.createElement('div');
+
+    elementReference.current = div;
+  }
+
+  useEffect(() => {
+    const modalRoot = document.getElementById('modal');
+
+    modalRoot.appendChild(elementReference.current);
+
+    return () => modalRoot.removeChild(elementReference.current);
+  }, []);
+
+  return createPortal(<div>{children}</div>, elementReference.current);
+};
+
+export default Modal;
