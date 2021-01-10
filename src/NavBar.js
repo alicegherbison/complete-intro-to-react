aca90c1aa6776c2213c3ed8d6d0dfe31b8cf4380
alicegherbison <git@alicegherbison.com>
2021-01-10 20:50:19 +0000

    chore: add emotion

--- src/NavBar.js
+++ src/NavBar.js
@@ -1,8 +1,18 @@
-import React from "react";
 import { Link } from "@reach/router";
+/** @jsx jsx */
+import { css, jsx } from "@emotion/react";
 
 const NavBar = () => (
-  <header>
+  <header
+    css={css`
+      padding: 32px;
+      background-color: hotpink;
+      font-size: 24px;
+      border-radius: 4px;
+      &:hover {
+        color: pink;
+      }
+    `}>
     <Link className="title" to="/">
       Adopt me{" "}
       <span aria-label="logo" role="img">
