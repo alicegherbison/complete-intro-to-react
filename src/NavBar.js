a5f94cfcbe1863311a51ea330449ab17535ea439
alicegherbison <git@alicegherbison.com>
2021-02-19 16:46:30 +0000

    style: add animation

--- src/NavBar.js
+++ src/NavBar.js
@@ -1,18 +1,32 @@
 import { Link } from "@reach/router";
 /** @jsx jsx */
-import { css, jsx } from "@emotion/react";
+import { css, jsx, keyframes } from "@emotion/react";
+
+const spin = keyframes`to {
+  transform: rotate(360deg);
+}`;
 
 const NavBar = () => (
   <header
     css={css`
+      border-radius: var(--radius);
       padding: 3.2rem;
       background-color: var(--color-lilac);
       font-size: 2.4rem;
-      border-radius: 0.4rem;
     `}>
     <Link className="title" to="/">
       Adopt me{" "}
-      <span aria-label="logo" role="img">
+      <span
+        aria-label="logo"
+        css={css`
+          display: inline-block;
+          animation: 1s ${spin} linear infinite;
+
+          &:hover {
+            animation: 1s ${spin} linear infinite reverse;
+          }
+        `}
+        role="img">
         🌈
       </span>
     </Link>
