91e30a62177877421f4e634cc928bf3fb1921131
alicegherbison <git@alicegherbison.com>
2021-03-02 16:04:56 +0000

    fix: results text columns

--- src/components/Results.tsx
+++ src/components/Results.tsx
@@ -9,12 +9,12 @@ interface IProps {
 
 const Results: FunctionComponent<IProps> = ({ pets }) => {
   return (
-    <section className="results" data-testid="results">
+    <section className="content" data-testid="results">
       {pets === null ? (
         <h2>Use the search form to search for your ideal pet.</h2>
       ) : pets.length ? (
-        pets.map((pet) => {
-          return (
+        <div className="results">
+          {pets.map((pet) => (
             <Pet
               breed={pet.breeds.primary}
               id={pet.id}
@@ -23,8 +23,8 @@ const Results: FunctionComponent<IProps> = ({ pets }) => {
               media={pet.photos}
               name={pet.name}
             />
-          );
-        })
+          ))}
+        </div>
       ) : (
         <h2>No pets found, try searching again.</h2>
       )}
